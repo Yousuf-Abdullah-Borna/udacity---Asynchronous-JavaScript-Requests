94e4aa4138367beabc6ca18f696b8e0c36ec44c7
Yousuf Borno <yb.26.code@gmail.com>
2020-12-29 08:13:14 -0500

    git table created for displaying data

--- lesson0/practice2.js
+++ lesson0/practice2.js
@@ -15,21 +15,50 @@ function handleSuccess () {
     
     responseData =data;
 
-    data.forEach(element => {
-        console.log("User Id: "+ element.id + ", Account created: " +element.actor.avatar_url);
-    });
-
+    //HEADING FOR THE HTML PAGE TO DISPLAY THE INFORMATION
     let header1 = document.createElement('h1');
     header1.style.color = 'red';
     header1.style.textAlign = 'center';
     header1.innerHTML= "Github Profiles";
     document.body.append(header1)
 
+    //FOR TABLE ELEMENT
+    let table = document.createElement('table');
+    table.style.border = '1px solid black';
+    table.style.borderCollapse ='collapse';
+    table.style.width ='400px';
+    
+    table.style.marginLeft= '38%';
+
+    //FOR TABLE COLUMN HEADERS
+    let trow = document.createElement('tr');
+    let theadOne = document.createElement('th');
+    theadOne.innerHTML ="userId";
+    let theadTwo = document.createElement('th');
+    theadTwo.innerHTML ="Avatar";
+    trow.append(theadOne)
+    trow.append(theadTwo)
+    table.append(trow)
+
+    document.body.append(table);
+
+    data.forEach(element => {
+        console.log("User Id: "+ element.id + ", Account created: " +element.actor.avatar_url);
+     
+         
+         
+
+    });
+
+    
+
   }
   
   function handleError () { 
     console.log( 'An error occurred \uD83D\uDE1E' );
   }
+
+  
 
 
 //CONSTRUCTED THE XMLHttpRequest
